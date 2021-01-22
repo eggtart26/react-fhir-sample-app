b953d5a2f408674f2018c3082d99598858bc6b89
eggtart26 <justin.guan26@gmail.com>
2021-01-22 00:43:27 -0800

    set up firebase Authentication

---
+++ src/Login/fire_example.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    /*
+    Replace it with your firebaseConfig.
+    */
+};
+// Initialize Firebase
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
